a4b2ea47380f8b6e767f260b62a566e9598457d9
Uswa-e-fatima <116622107+UswaeFatima@users.noreply.github.com>
2023-01-20 22:39:03 +0500

    Add files via upload

---
+++ avg.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+	int a = 28;
+    int b = 32;
+    int c = 37;
+	int d = 24;
+    int e = 33;
+    int sum,average;
+        sum = a + b + c + d + e,
+        average = sum / 5;
+    cout << "The average is " << average;
+    cout << endl << endl;
+    return 0;
+}
